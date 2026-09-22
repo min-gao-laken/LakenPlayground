4fa349cc74d7201a277c8e36f5b5241405c0c5fa
Mia <gliveitup@gmail.com>
2026-09-22 09:08:05 -0600

    C#: FitnessTracker/FitnessTrackerAPI - update

--- ASP.NET/FitnessTracker/frontend/fitness-tracker-ui/vite.config.js
+++ ASP.NET/FitnessTracker/frontend/fitness-tracker-ui/vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const apiTarget = env.VITE_API_TARGET || "http://localhost:5095";
-
+  // const apiTarget = env.VITE_API_TARGET || "http://localhost:5095";
+  const apiTarget = env.VITE_API_TARGET || "http://host.docker.internal:5095";
   return {
     plugins: [react()],
     server: {
